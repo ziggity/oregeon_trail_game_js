8801fc2bffcea6a1a4ae708d92f245f57234b2b1
zach beecher <zachbeecher@gmail.com>
2017-11-19 14:18:10 -0800

    add game js file which has the constants we'll need for the game object

---
+++ js/Caravan.js
@@ -0,0 +1,13 @@
+var OregonH = OregonH || {};
+
+OregonH.Caravan = {};
+
+OregonH.Caravan.init = function(stats){
+ this.day = stats.day;
+ this.distance = stats.distance;
+ this.crew = stats.crew;
+ this.food = stats.food;
+ this.oxen = stats.oxen;
+ this.money = stats.money;
+ this.firepower = stats.firepower;
+};
