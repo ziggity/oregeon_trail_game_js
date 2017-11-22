de9b25013231bd5c4a3931a0744d14778d1f57c1
zach beecher <zachbeecher@gmail.com>
2017-11-21 23:59:21 -0800

    add shops to event js

--- js/Event.js
+++ js/Event.js
@@ -60,6 +60,39 @@ OregonH.Event.eventTypes = [
    text: 'Found wild oxen. New oxen: '
  },
  {
+   type: 'SHOP',
+   notification: 'neutral',
+   text: 'You have found a shop',
+   products: [
+     {item: 'food', qty: 20, price: 50},
+     {item: 'oxen', qty: 1, price: 200},
+     {item: 'firepower', qty: 2, price: 50},
+     {item: 'crew', qty: 5, price: 80}
+   ]
+ },
+ {
+   type: 'SHOP',
+   notification: 'neutral',
+   text: 'You have found a shop',
+   products: [
+     {item: 'food', qty: 30, price: 50},
+     {item: 'oxen', qty: 1, price: 200},
+     {item: 'firepower', qty: 2, price: 20},
+     {item: 'crew', qty: 10, price: 80}
+   ]
+ },
+ {
+   type: 'SHOP',
+   notification: 'neutral',
+   text: 'Smugglers sell various goods',
+   products: [
+     {item: 'food', qty: 20, price: 60},
+     {item: 'oxen', qty: 1, price: 300},
+     {item: 'firepower', qty: 2, price: 80},
+     {item: 'crew', qty: 5, price: 60}
+   ]
+ },
+ {
    type: 'ATTACK',
    notification: 'negative',
    text: 'Bandits are attacking you'
@@ -86,6 +119,18 @@ OregonH.Event.generateEvent = function(){
    this.stateChangeEvent(eventData);
  }
 
+ //shops
+ else if(eventData.type == 'SHOP') {
+   //pause game
+   this.game.pauseJourney();
+
+   //notify user
+   this.ui.notify(eventData.text, eventData.notification);
+
+   //prepare event
+   this.shopEvent(eventData);
+ }
+
  //attacks
  else if(eventData.type == 'ATTACK') {
    //pause game
@@ -101,12 +146,36 @@ OregonH.Event.generateEvent = function(){
 
 OregonH.Event.stateChangeEvent = function(eventData) {
  //can't have negative quantities
- if(eventData.value + this.caravan[eventData.stat] &gt;= 0) {
+ if(eventData.value + this.caravan[eventData.stat] >= 0) {
    this.caravan[eventData.stat] += eventData.value;
    this.ui.notify(eventData.text + Math.abs(eventData.value), eventData.notification);
  }
 };
 
+OregonH.Event.shopEvent = function(eventData) {
+ //number of products for sale
+ var numProds = Math.ceil(Math.random() * 4);
+
+ //product list
+ var products = [];
+ var j, priceFactor;
+
+ for(var i = 0; i < numProds; i++) {
+   //random product
+   j = Math.floor(Math.random() * eventData.products.length);
+
+   //multiply price by random factor +-30%
+   priceFactor = 0.7 + 0.6 * Math.random();
+
+   products.push({
+     item: eventData.products[j].item,
+     qty: eventData.products[j].qty,
+     price: Math.round(eventData.products[j].price * priceFactor)
+   });
+ }
+
+ this.ui.showShop(products);
+};
 
 //prepare an attack event
 OregonH.Event.attackEvent = function(eventData){
