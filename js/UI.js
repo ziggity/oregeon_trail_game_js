e91f2a97d800ce623731d990b3419d0978911c5e
zach beecher <zachbeecher@gmail.com>
2017-11-21 23:51:01 -0800

    dom manipulation with the ui js

--- js/UI.js
+++ js/UI.js
@@ -4,11 +4,21 @@ OregonH.UI = {};
 
 //show a notification in the message area
 OregonH.UI.notify = function(message, type){
- console.log(message + ' - ' + type);
+ document.getElementById('updates-area').innerHTML = '<div class="update-' + type + '">Day '+ Math.ceil(this.caravan.day) + ': ' + message+'</div>' + document.getElementById('updates-area').innerHTML;
 };
-
 
 //refresh visual caravan stats
 OregonH.UI.refreshStats = function() {
- console.log(this.caravan);
-}
+ //modify the dom
+ document.getElementById('stat-day').innerHTML = Math.ceil(this.caravan.day);
+ document.getElementById('stat-distance').innerHTML = Math.floor(this.caravan.distance);
+ document.getElementById('stat-crew').innerHTML = this.caravan.crew;
+ document.getElementById('stat-oxen').innerHTML = this.caravan.oxen;
+ document.getElementById('stat-food').innerHTML = Math.ceil(this.caravan.food);
+ document.getElementById('stat-money').innerHTML = this.caravan.money;
+ document.getElementById('stat-firepower').innerHTML = this.caravan.firepower;
+ document.getElementById('stat-weight').innerHTML = Math.ceil(this.caravan.weight) + '/' + this.caravan.capacity;
+
+ //update caravan position
+ document.getElementById('caravan').style.left = (380 * this.caravan.distance/OregonH.FINAL_DISTANCE) + 'px';
+};
