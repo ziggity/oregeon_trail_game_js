8200f8f60c2b333cac6ab4ed3df130ecc0ea8a28
zach beecher <zachbeecher@gmail.com>
2017-11-21 23:59:59 -0800

    final touches to ui js

--- js/UI.js
+++ js/UI.js
@@ -1,24 +1,67 @@
-var OregonH = OregonH || {};
-
-OregonH.UI = {};
-
-//show a notification in the message area
-OregonH.UI.notify = function(message, type){
- document.getElementById('updates-area').innerHTML = '<div class="update-' + type + '">Day '+ Math.ceil(this.caravan.day) + ': ' + message+'</div>' + document.getElementById('updates-area').innerHTML;
-};
-
-//refresh visual caravan stats
-OregonH.UI.refreshStats = function() {
- //modify the dom
- document.getElementById('stat-day').innerHTML = Math.ceil(this.caravan.day);
- document.getElementById('stat-distance').innerHTML = Math.floor(this.caravan.distance);
- document.getElementById('stat-crew').innerHTML = this.caravan.crew;
- document.getElementById('stat-oxen').innerHTML = this.caravan.oxen;
- document.getElementById('stat-food').innerHTML = Math.ceil(this.caravan.food);
- document.getElementById('stat-money').innerHTML = this.caravan.money;
- document.getElementById('stat-firepower').innerHTML = this.caravan.firepower;
- document.getElementById('stat-weight').innerHTML = Math.ceil(this.caravan.weight) + '/' + this.caravan.capacity;
-
- //update caravan position
- document.getElementById('caravan').style.left = (380 * this.caravan.distance/OregonH.FINAL_DISTANCE) + 'px';
-};
+//show shop
+OregonH.UI.showShop = function(products){
+    
+     //get shop area
+     var shopDiv = document.getElementById('shop');
+     shopDiv.classList.remove('hidden');
+    
+     //init the shop just once
+     if(!this.shopInitiated) {
+    
+       //event delegation
+       shopDiv.addEventListener('click', function(e){
+         //what was clicked
+         var target = e.target || e.src;
+    
+         //exit button
+         if(target.tagName == 'BUTTON') {
+           //resume journey
+           shopDiv.classList.add('hidden');
+           OregonH.UI.game.resumeJourney();
+         }
+         else if(target.tagName == 'DIV' && target.className.match(/product/)) {
+    
+           OregonH.UI.buyProduct({
+             item: target.getAttribute('data-item'),
+             qty: target.getAttribute('data-qty'),
+             price: target.getAttribute('data-price')
+           });
+    
+         }
+       });
+    
+       this.shopInitiated = true;
+     }
+    
+     //clear existing content
+     var prodsDiv = document.getElementById('prods');
+     prodsDiv.innerHTML = '';
+    
+     //show products
+     var product;
+     for(var i=0; i < products.length; i++) {
+       product = products[i];
+       prodsDiv.innerHTML += '<div class="product" data-qty="' + product.qty + '" data-item="' + product.item + '" data-price="' + product.price + '">' + product.qty + ' ' + product.item + ' - $' + product.price + '</div>';
+     }
+   };
+    
+   //buy product
+   OregonH.UI.buyProduct = function(product) {
+     //check we can afford it
+     if(product.price > OregonH.UI.caravan.money) {
+       OregonH.UI.notify('Not enough money', 'negative');
+       return false;
+     }
+    
+     OregonH.UI.caravan.money -= product.price;
+    
+     OregonH.UI.caravan[product.item] += +product.qty;
+    
+     OregonH.UI.notify('Bought ' + product.qty + ' x ' + product.item, 'positive');
+    
+     //update weight
+     OregonH.UI.caravan.updateWeight();
+    
+     //update visuals
+     OregonH.UI.refreshStats();
+   };
